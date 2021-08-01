aee959ccdb290dc8a7eb81538e1243cb4aea1554
oubin <oubin.ob@antgroup.com>
2021-08-01 15:55:37 +0800

    init

---
+++ src/main/java/com/ob/thinkinginspringboot/ThinkingInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ob.thinkinginspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkingInSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ob/thinkinginspringboot/ThinkingInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ob.thinkinginspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
