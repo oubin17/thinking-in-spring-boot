f3d0de6171b6bc466634d77831cb7f4618fc0dd7
oubin <oubin.ob@antgroup.com>
2021-08-04 23:44:52 +0800

    request

--- src/main/java/com/ob/thinkinginspringboot/ThinkingInSpringBootApplication.java
+++ src/main/java/com/ob/thinkinginspringboot/ThinkingInSpringBootApplication.java
@@ -2,12 +2,24 @@ package com.ob.thinkinginspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class ThinkingInSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThinkingInSpringBootApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> helloWorld() {
+		return route(GET("hello-world"), request -> ServerResponse.ok().body(
+				"hello-world"));
 	}
 
 }
