e2da196c74e7ed038b2ceb8b8aa54f8ec79a7b23
oubin <oubin.ob@antgroup.com>
2021-08-21 22:18:40 +0800

    conditional

---
+++ src/main/java/com/ob/thinkinginspringboot/conditional/ConditionOnSystemProperty.java
@@ -0,0 +1,30 @@
+package com.ob.thinkinginspringboot.conditional;
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.*;
+
+/**
+ * ConditionOnSystemProperty
+ *
+ * @author oubin.ob
+ * @version : ConditionOnSystemProperty.java v 0.1 2021/8/21 21:59 oubin.ob Exp $$
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Conditional(OnSystemPropertyCondition.class)
+public @interface ConditionOnSystemProperty {
+
+    /**
+     *
+     * @return System 属性名称
+     */
+    String name();
+
+    /**
+     *
+     * @return System 属性值
+     */
+    String value();
+}
---
+++ src/main/java/com/ob/thinkinginspringboot/conditional/ConditionalOnSystemPropertyBootstrap.java
@@ -0,0 +1,24 @@
+package com.ob.thinkinginspringboot.conditional;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * ConditionalOnSystemPropertyBootstrap
+ *
+ * @author oubin.ob
+ * @version : ConditionalOnSystemPropertyBootstrap.java v 0.1 2021/8/21 22:14 oubin.ob Exp $$
+ */
+public class ConditionalOnSystemPropertyBootstrap {
+
+    static {
+        System.setProperty("language", "English");
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
+        configApplicationContext.register(ConditionalMessageConfiguration.class);
+        configApplicationContext.refresh();
+        String message = configApplicationContext.getBean("message", String.class);
+        System.out.printf("\"message\" Bean 对象 ：%s\n", message);
+    }
+}
